87e7cfaf8dc1dce9e9140d735532056cf09875d7
Данил <danikkach666@gmail.com>
2024-05-02 19:32:32 +0300

    add

--- src/generics/1.ts
+++ src/generics/1.ts
@@ -3,7 +3,7 @@
 
 import axios from "axios";
 
-async function fetchData(url: string): Promise<any> {
+async function fetchData<T>(url: string): Promise<T> {
   try {
     const response = await axios.get(url);
     return response.data;
